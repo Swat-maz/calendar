8a7dcdf6908d10c57e2adefd680ac21f3e27eb64
Swat-maz <igor.mazur.selets@gmail.com>
2021-08-28 19:29:08 +0300

    preLast comment

--- web/modules/custom/calendar/src/Form/CalendarForm.php
+++ web/modules/custom/calendar/src/Form/CalendarForm.php
@@ -103,7 +103,7 @@ class CalendarForm extends FormBase {
           $var = $headers[$c];
 
           if ($var === "Year" || $var === "Q1" || $var === "Q2" || $var === "Q3" || $var === "Q4" || $var === "YTD") {
-            $disable = TRUE;
+//            $disable = TRUE;
             if ($var === "Year") {
               $yearValue = date('Y', time() ) - $i+1;
               $form['wrapper'][$a][$yearValues][$var] = [
@@ -112,22 +112,13 @@ class CalendarForm extends FormBase {
                 '#default_value' => $yearValue,
                 '#title_display' => 'invisible',
               ];
-            }else {
+            }elseif ($var === "Q1" || $var === "Q2" || $var === "Q3" || $var === "Q4" || $var === "YTD") {
               $form['wrapper'][$a][$yearValues][$var] = [
                 '#type' => 'textfield',
                 '#disabled' => TRUE,
-                '#default_value' => '',
                 '#title_display' => 'invisible',
               ];
             }
-//            $paste = "'#disabled' => TRUE";
-//            $form['wrapper'][$a][$yearValues][$var] = [
-//              '#type' => 'number',
-//              //            '#disabled' => $disable,
-//              $paste,
-//              '#default_value' => $yearValue,
-//              '#title_display' => 'invisible',
-//            ];
           }else {
             $form['wrapper'][$a][$yearValues][$var] = [
               '#type' => 'number',
@@ -195,86 +186,150 @@ class CalendarForm extends FormBase {
   }
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
-//    parent::validateForm($form, $form_state);
-    $headers = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    $numOfTables = $form_state->getStorage()['num_of_tables'];
-    $numOfRow = $form_state->getStorage()['num_of_rows'];
+    //    parent::validateForm($form, $form_state);
+    $trigeringElement = $form_state->getTriggeringElement();
+    if ($trigeringElement['#value'] === "Submit") {
 
-    if ($numOfTables > 1) {
-      // витягування усіх заповнених даних в масив
-      for ($i = 0; $i < $numOfTables; $i++) {
-        $allValues[$i] = $form_state->getUserInput()['wrapper'][$i];
+      $headers = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+      ];
+      $numOfTables = $form_state->getStorage()['num_of_tables'];
+      $numOfRow = $form_state->getStorage()['num_of_rows'];
+
+      if ($numOfTables > 1) {
+        // витягування усіх заповнених даних в масив
+        for ($i = 0; $i < $numOfTables; $i++) {
+          $allValues[$i] = $form_state->getUserInput()['wrapper'][$i];
+        }
       }
-    }
 
-//    видаляє усі порожні значення з масивів
-//    for ($filterCounter = 0; $filterCounter < $numOfRow; $filterCounter ++) {
-//      $yearValues = date('Y', time() ) - $filterCounter;
-//      $filteredArray[$yearValues] = (array_filter($values[$yearValues]));
-//    }
+      //    видаляє усі порожні значення з масивів
+      //    for ($filterCounter = 0; $filterCounter < $numOfRow; $filterCounter ++) {
+      //      $yearValues = date('Y', time() ) - $filterCounter;
+      //      $filteredArray[$yearValues] = (array_filter($values[$yearValues]));
+      //    }
 
-    $simplCounter = 3;
-    $masuvNeSpivpadin=[];
-    for ($j = 1; $j <= $numOfTables-1; $j++) {
-      // форнікa для кількості рядків тобто для перевірки таблиць по роках
-      for ($numberOfYears = 0; $numberOfYears < $numOfRow; $numberOfYears++) {
-        $yearValues = date('Y', time() ) - $numberOfYears;
-        // форнікa для прирівняння чи однакові місяці в роках не порожні
-        for ($i = 0; $i < count($headers); $i++) {
-          $var = $headers[$i];
-          if ($allValues[0][$yearValues][$var] !== "" && $allValues[$j][$yearValues][$var] !== "") {
-            $simplCounter = 1;
-          }
-          elseif ($allValues[0][$yearValues][$var] == "" && $allValues[$j][$yearValues][$var] == "") {
-            $simplCounter = 1;
-          }
-          else {
-            $simplCounter = 0;
-            if ($allValues[0][$yearValues][$var] === "") {
-              $masuvNeSpivpadin[0][$yearValues][$var] = "$var";
+      $simplCounter = 3;
+      $masuvNeSpivpadin = [];
+      for ($j = 1; $j <= $numOfTables - 1; $j++) {
+        // форнікa для кількості рядків тобто для перевірки таблиць по роках
+        for ($numberOfYears = 0; $numberOfYears < $numOfRow; $numberOfYears++) {
+          $yearValues = date('Y', time()) - $numberOfYears;
+          // форнікa для прирівняння чи однакові місяці в роках не порожні
+          for ($i = 0; $i < count($headers); $i++) {
+            $var = $headers[$i];
+            if ($allValues[0][$yearValues][$var] !== "" && $allValues[$j][$yearValues][$var] !== "") {
+              $simplCounter = 1;
             }
-            elseif ($allValues[$j][$yearValues][$var] === "") {
-              $masuvNeSpivpadin[$j][$yearValues][$var] = "$var";
+            elseif ($allValues[0][$yearValues][$var] == "" && $allValues[$j][$yearValues][$var] == "") {
+              $simplCounter = 1;
+            }
+            else {
+              $simplCounter = 0;
+              if ($allValues[0][$yearValues][$var] === "") {
+                $masuvNeSpivpadin[0][$yearValues][$var] = "$var";
+              }
+              elseif ($allValues[$j][$yearValues][$var] === "") {
+                $masuvNeSpivpadin[$j][$yearValues][$var] = "$var";
+              }
             }
           }
         }
       }
-    }
-    // ерром месейдж при не однаковому заповненні таблиць
-    if (!empty($masuvNeSpivpadin)) {
-      return $form_state->setErrorByName('title', $this->t("Invalid"));
-    }
+      // ерром месейдж при не однаковому заповненні таблиць
+      if (!empty($masuvNeSpivpadin)) {
+        return $form_state->setErrorByName('title', $this->t("Invalid"));
+      }
 
-    // валідація на нерозривність вводу
-    if ($numOfRow >= 1) {
-      $allValues = $form_state->getUserInput()['wrapper'][0];
-      for ($key = 1; $key <= $numOfRow*12; $key++) {
-        $array_keys[] = $key;
-      }
-      foreach ($allValues as $values){
-        foreach ($values as $value){
-          $firstArray[] = $value;
+      // валідація на нерозривність вводу
+      if ($numOfRow >= 1) {
+        $allValues = $form_state->getUserInput()['wrapper'][0];
+        for ($key = 1; $key <= $numOfRow * 12; $key++) {
+          $array_keys[] = $key;
+        }
+        foreach ($allValues as $values) {
+          foreach ($values as $value) {
+            $firstArray[] = $value;
+          }
+        }
+        for ($firstCounter = 0; $firstCounter < count($array_keys); $firstCounter++) {
+          $newAllValues[$array_keys[$firstCounter]] = $firstArray[$firstCounter];
+        }
+        $newAllValuesFiltered = array_filter($newAllValues);
+        $arrayAllValuesKeys = array_keys($newAllValuesFiltered);
+        for ($k = 0; $k < count($arrayAllValuesKeys) - 1; $k++) {
+          if ($arrayAllValuesKeys[$k] + 1 != $arrayAllValuesKeys[$k + 1]) {
+            return $form_state->setErrorByName('title', $this->t("Invalid"));
+          }
         }
       }
-      for ($firstCounter = 0; $firstCounter < count($array_keys); $firstCounter++) {
-        $newAllValues[$array_keys[$firstCounter]] =  $firstArray[$firstCounter];
-      }
-      $newAllValuesFiltered = array_filter($newAllValues);
-      $arrayAllValuesKeys = array_keys($newAllValuesFiltered);
-      for ($k = 0; $k < count($arrayAllValuesKeys)-1; $k++){
-        if ($arrayAllValuesKeys[$k]+1 != $arrayAllValuesKeys[$k+1]){
-          return $form_state->setErrorByName('title', $this->t("Invalid"));
-        }
-      }
+      return $this->messenger()->addStatus($this->t('Valid'));
     }
-    return $this->messenger()->addStatus($this->t('Valid'));
   }
 
   /**
    *
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $numOfTables = $form_state->getStorage()['num_of_tables'];
+    $numOfRow = $form_state->getStorage()['num_of_rows'];
+    for ($i = 0; $i < $numOfTables; $i++) {
+      $allValues[$i] = $form_state->getUserInput()['wrapper'][$i];
+    }
+    foreach ($allValues as $key => $tables){
+      foreach ($tables as $years){
+        $chunkedArray[$key][] = array_chunk($years, 3);
+      }
+    }
+    foreach ($chunkedArray as $key => $tables){
+      foreach ($tables as $yearKey => $years){
+          foreach ($years as $quarterKey => $quarter){
+            array_sum($quarter) == 0 ?
+              $quarterSumArray[$key][$yearKey][$quarterKey] = array_sum($quarter) :
+              $quarterSumArray[$key][$yearKey][$quarterKey] = round((array_sum($quarter)+1)/3, 2);
+          }
+      }
+    }
+    foreach ($quarterSumArray as $key => $tables){
+      foreach ($tables as $yearKey => $years){
+          array_sum($years) == 0 ?
+            $yearSumArray[$key][$yearKey] = array_sum($years) :
+            $yearSumArray[$key][$yearKey] = round((array_sum($years)+1)/4, 2);
+      }
+    }
+    $quarters = ['Q1', 'Q2', 'Q3', 'Q4'];
+    for ($j = $numOfRow; $j > 0; $j--){
+      $yearArray[] = date('Y', time() ) - $j+1;
+    }
+    foreach ($quarterSumArray as $key => $tables){
+      foreach ($tables as $yearKey => $years){
+        foreach ($years as $quarterKey => $quarter)
+          $quarter != 0 ? $form['wrapper'][$key][$yearArray[$yearKey]][$quarters[$quarterKey]]['#value'] = $quarter :
+            $form['wrapper'][$key][$yearArray[$yearKey]][$quarters[$quarterKey]]['#value'] = "";
+        }
+      }
+    foreach ($yearSumArray as $key => $tables){
+      foreach ($tables as $yearKey => $years){
+        $years != 0 ? $form['wrapper'][$key][$yearArray[$yearKey]]['YTD']['#value'] = $years :
+            $form['wrapper'][$key][$yearArray[$yearKey]]['YTD']['#value'] = "";
+      }
+    }
+//    $submitReturn['year'] = $yearSumArray;
+//    $submitReturn['quarter'] = $quarterSumArray;
 
+    return $form;
+//    return $form['wrapper'][0][2021]['Q1']['#value'] = 10;
   }
 
 
